4d7a61e70cc30e58f29a8a28f2598e37467a9bc1
mohye <mohyeddine-mohamad@hotmail.com>
2018-05-27 13:43:26 +0300

    storage

--- app/src/main/java/com/master1/newsapplication/androidproject/Storage.java
+++ app/src/main/java/com/master1/newsapplication/androidproject/Storage.java
@@ -1,71 +1,76 @@
 package com.master1.newsapplication.androidproject;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
 import android.support.annotation.NonNull;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Storage {
     private Context context;
     private StorageReference mStorageRef;
+    static File dir;
+    FullNews news;
+    String path;
 
-    public Storage(Context context) {
+    public Storage(Context context,FullNews news) {
         mStorageRef = FirebaseStorage.getInstance().getReference();
+        this.context=context;
+        this.news=news;
+        if(dir==null)
+            dir=new File(context.getFilesDir(),"news");
+        path=dir.getAbsolutePath()+File.pathSeparator+news.getCategorie()+File.pathSeparator+news.getId();
     }
-    public void getImage(final ImageView view) throws IOException {
 
-        final File localFile = File.createTempFile("news0", "png");
-        //Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
-        StorageReference riversRef = mStorageRef.child("news/news0.png");
-        //Toast.makeText(context, riversRef.getName(), Toast.LENGTH_SHORT).show();
+    public void DownloadAll()
+    {
+
+    }
+
+    public void DownloadMain() throws IOException {
+
+        final File localFile = new File(path+File.pathSeparator+"main");
+        StorageReference riversRef = mStorageRef.child(news.getPathMainPhot());
         riversRef.getFile(localFile)
                 .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                        Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                        //Bitmap bitmap = Bitmap.createBitmap(bm, 0, 0,400, 400, null, true);
-                        view.setImageBitmap(bitmap);
-                        System.out.println(taskSnapshot.getTotalByteCount());
+                        news.setMainPhoto(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
                     }
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
             }
         });
-        /*Glide.with(context)
-                .using(new FirebaseImageLoader())
-                .load(riversRef)
-                .into(view);*/
+    }
 
-        riversRef.getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-            @Override
-            public void onSuccess(byte[] bytes) {
-                // Use the bytes to display the image
-                System.out.println(bytes.length);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle any errors
-            }
-        });
+    public void DownloadSub() throws IOException {
 
+        ArrayList<String> paths=getMn7sen();
+        StorageReference dir = mStorageRef.child(news.getPathPhotos());
+        for(String mp:paths)
+        {
+            StorageReference riversRef = dir.child(mp);
+            final File localFile = new File(path+File.pathSeparator+"Photos"+File.pathSeparator+mp);
+            riversRef.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            news.addPhoto(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                }
+            });
+        }
     }
 }
