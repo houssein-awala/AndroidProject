2d580257d8fd8e8d354f3ee923f306a71efd4a4b
LENOVO <ghinasaad12@gmail.com>
2018-05-27 00:21:22 -0700

    controlpanel

--- app/src/main/java/com/master1/newsapplication/androidproject/ControlPanel.java
+++ app/src/main/java/com/master1/newsapplication/androidproject/ControlPanel.java
@@ -2,24 +2,39 @@ package com.master1.newsapplication.androidproject;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.skydoves.colorpickerpreference.ColorEnvelope;
+import com.skydoves.colorpickerpreference.ColorListener;
+import com.skydoves.colorpickerpreference.ColorPickerDialog;
+import com.skydoves.colorpickerpreference.ColorPickerPreference;
+import com.skydoves.colorpickerpreference.ColorPickerView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -38,6 +53,51 @@ public class ControlPanel extends AppCompatPreferenceActivity {
      * A preference value change listener that updates the preference's summary
      * to reflect its new value.
      */
+   public static String background_color,toolbar_color,s,f;
+   static Set<String> notifications;
+    static Set<String> saved;
+   static ArrayList<String> categorie=new ArrayList<>();
+   public static ArrayList<String> getcategories()
+   {
+
+       categorie.add("Sport");
+       categorie.add("Arts");
+       categorie.add("Policy");
+       categorie.add("Economie");
+       return categorie;
+   }
+   public ControlPanel()
+   {}
+   public ControlPanel(String background_color,String toolbar_color,Set<String> notifications)
+   {
+       this.background_color=background_color;
+       this.toolbar_color=toolbar_color;
+       this.notifications=notifications;
+   }
+   public String getbackgroundcolor()
+   {
+       return background_color;
+   }
+   public String gettoolbarcolor()
+   {
+       return toolbar_color;
+   }
+   public void setBackground_color(String background_color)
+   {
+       this.background_color=background_color;
+   }
+   public void setToolbar_color(String toolbar_color)
+   {
+       this.toolbar_color=toolbar_color;
+   }
+   public Set<String> getNotifications()
+   {
+       return saved;
+   }
+   public void setNotifications(Set<String> notifications)
+   {
+       this.notifications=notifications;
+   }
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
         public boolean onPreferenceChange(Preference preference, Object value) {
@@ -198,12 +258,95 @@ public class ControlPanel extends AppCompatPreferenceActivity {
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class NotificationPreferenceFragment extends PreferenceFragment {
+        SharedPreferences pref,sharedPreferences;
+        SharedPreferences.Editor editor;
+        public static final String Mypref="pref";
+        public static String color_back="color_back";
+        public static String color_tool="color_tool";
+        public ColorPickerPreference colorPickerPreferencebackground;
+        public ColorPickerPreference colorPickerPreferencetoolbar;
+        @RequiresApi(api = Build.VERSION_CODES.M)
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_notification);
             setHasOptionsMenu(true);
+            sharedPreferences=getPreferenceManager().getSharedPreferences();
+            ColorPickerView colorPickerView;
 
+            colorPickerPreferencebackground=(ColorPickerPreference)getPreferenceManager().findPreference("BackgroundColorPickerPreference");
+            ColorPickerDialog.Builder builder=colorPickerPreferencebackground.getColorPickerDialogBuilder();
+
+            colorPickerView = builder.getColorPickerView();
+            colorPickerView.setColorListener(new ColorListener() {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onColorSelected(ColorEnvelope colorEnvelope) {
+                    int a = colorEnvelope.getColor();
+                    s=colorEnvelope.getColorHtml();
+                    Toast.makeText(getContext(),s,Toast.LENGTH_SHORT).show();
+                }
+            });
+            // colorPickerView.setFlagView(new CustomFlag(this.getContext(), R.layout.layout_flag));
+            // colorPickerView.setFlagMode(FlagMode.ALWAYS);
+            //   pref=getPreferenceManager().getSharedPreferences();
+            background_color=colorPickerView.getSavedColorHtml(Color.WHITE);
+            pref = getActivity().getSharedPreferences(Mypref,Context.MODE_PRIVATE);
+            editor=pref.edit();
+            editor.putString(color_back,background_color);
+            editor.commit();
+            Toast.makeText(this.getContext(),"hiii"+pref.getString(color_back,""),Toast.LENGTH_SHORT).show();
+
+            builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+                @Override
+                public void onCancel(DialogInterface dialog) {
+
+                    dialog.dismiss();
+                }
+            });
+            //Toolbar
+            colorPickerPreferencetoolbar=(ColorPickerPreference)getPreferenceManager().findPreference("ToolbarColorPickerPreference");
+            ColorPickerDialog.Builder builder1=colorPickerPreferencetoolbar.getColorPickerDialogBuilder();
+            // builder.setFlagView(new CustomFlag(this,R.layout.la));
+            colorPickerView = builder1.getColorPickerView();
+            colorPickerView.setColorListener(new ColorListener() {
+                @Override
+                public void onColorSelected(ColorEnvelope colorEnvelope) {
+                    int a = colorEnvelope.getColor();
+                    f=colorEnvelope.getColorHtml();
+                    Toast.makeText(getContext(),f,Toast.LENGTH_SHORT).show();
+                }
+            });
+            //     colorPickerView.setFlagView(new CustomFlag(this.getContext(), R.layout.layout_flag));
+            //      colorPickerView.setFlagMode(FlagMode.ALWAYS);
+            pref=getPreferenceManager().getSharedPreferences();
+            toolbar_color=colorPickerView.getSavedColorHtml(Color.WHITE);
+            pref = getActivity().getSharedPreferences(Mypref,Context.MODE_PRIVATE);
+            editor=pref.edit();
+            editor.putString(color_tool,toolbar_color);
+            editor.commit();
+            builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+                @Override
+                public void onCancel(DialogInterface dialog) {
+
+                    dialog.dismiss();
+                }
+            });
+            //notification
+            notifications=new HashSet<>();
+            categorie=getcategories();
+            for (String region : categorie)
+                notifications.add(region);
+
+            MultiSelectListPreference list=(MultiSelectListPreference)getPreferenceManager().findPreference("categorie");
+            saved=sharedPreferences.getStringSet("categorie", (Set<String>) notifications);
+            Set<String> cat=notifications;
+            CharSequence[] sequences = cat.toArray(new CharSequence[cat.size()]);
+
+            list.setEntries(sequences);
+            list.setEntryValues(sequences);
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
